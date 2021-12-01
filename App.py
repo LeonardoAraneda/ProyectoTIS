060bf615f72131baf32f17fcb59804a115182003
LA4M <leonardoaranedam@gmail.com>
2021-12-01 16:07:21 -0300

    Comienzo de interfaz

--- App.py
+++ App.py
@@ -5,6 +5,19 @@ import psycopg2
 import psycopg2.extras
 
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+posts = []
+
+@app.route("/")
+def index():
+    return render_template("index.html", num_posts=len(posts))
+
+
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return render_template("post_view.html", slug_title=slug)
+
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+def post_form(post_id=None):
+    return render_template("admin/post_form.html", post_id=post_id)
