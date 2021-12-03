0cd0ce9ac827bbffc057d69fb507f1e5df95b348
LA4M <leonardoaranedam@gmail.com>
2021-12-03 11:01:02 -0300

    Testeo de caracteristicas

--- App.py
+++ App.py
@@ -3,15 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import psycopg2
 import psycopg2.extras
-from forms import SignupForm, Cancha
+from forms import SignupForm, Cancha, PostForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 
+
+
 posts = []
 @app.route("/")
 def index():
-    return render_template("index.html", num_posts=len(posts))
+    return render_template("index.html", posts=posts)
 
 @app.route("/reserva_completa/")
 def reserva_completa():
@@ -21,11 +23,20 @@ def reserva_completa():
 def show_post(slug):
     return render_template("post_view.html", slug_title=slug)
 
+@app.route("/admin/post/", methods=['GET', 'POST'], defaults={'post_id': None})
+@app.route("/admin/post/<int:post_id>/", methods=['GET', 'POST'])
+def post_form(post_id):
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        title_slug = form.title_slug.data
+        content = form.content.data
 
-@app.route("/admin/post/")
-@app.route("/admin/post/<int:post_id>/")
-def post_form(post_id=None):
-    return render_template("admin/post_form.html", post_id=post_id)
+        post = {'title': title, 'title_slug': title_slug, 'content': content}
+        posts.append(post)
+
+        return redirect(url_for('index'))
+    return render_template("admin/post_form.html", form=form)
 
 
 @app.route("/signup/", methods=["GET", "POST"])
